a58b267c99f0f6bbdd32ab55dbe4b5dbd20f8e9f
ToshiroZ <48633791+ToshiroZ@users.noreply.github.com>
2020-02-04 12:40:11 -0500

    Add project files.

---
+++ Discord Webhook/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Discord.Webhook
+{
+    public class Webhook
+    {
+        private readonly string _url;
+        private readonly string _username = "Toshi's Webhook Library";
+        private readonly string _avatarurl;
+
+        public Webhook(string webhookurl, string username = null, string avatarurl = null)
+        {
+            _url = webhookurl;
+            _username = username;
+            _avatarurl = avatarurl;
+        }
+
+        public void Send(string message)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                Dictionary<string, string> contents = new Dictionary<string, string>
+                    {
+                        { "content", message },
+                        { "username", _username },
+                        { "avatar_url", _avatarurl }
+                    };
+
+                client.PostAsync(_url, new FormUrlEncodedContent(contents)).GetAwaiter().GetResult();
+            }
+            catch { }
+        }
+    }
+}
